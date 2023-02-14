751fd8a7d65ee0ca21301debe391bf35d549a310
williamericcheung <williamericcheung@outlook.com>
2023-02-14 10:06:42 +0800

    fix

--- src/tech/wec/Facebook/MoveZeroes_283.java
+++ src/tech/wec/Facebook/MoveZeroes_283.java
@@ -2,17 +2,14 @@ package tech.wec.Facebook;
 
 public class MoveZeroes_283 {
     public void moveZeroes(int[] nums) {
-        int cntZero = 0;
-        int loc = 0;
-        for (int i =0;i<nums.length;i++){
-            if (nums[i] == 0){
-                cntZero++;
-            }else{
-                nums[loc++] = nums[i];
+        int len = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[len++] = nums[i];
             }
         }
-        for (int i=nums.length-1, j = cntZero;j>0;j--){
-            nums[i--] = 0;
+        for(int i = len; i < nums.length; i++) {
+            nums[i] = 0;
         }
     }
 }
