f1adc3f26d3e04f19668e6e8c26bb1ca3b3ec177
williamericcheung <williamericcheung@outlook.com>
2023-02-14 10:29:11 +0800

    fix

--- src/tech/wec/Facebook/SquaresOfASortedArray_977.java
+++ src/tech/wec/Facebook/SquaresOfASortedArray_977.java
@@ -3,33 +3,20 @@ package tech.wec.Facebook;
 public class SquaresOfASortedArray_977 {
 
     // very much like merge two sorted linked list
-    public int[] sortedSquares(int[] A) {
-        // pointer of non-negative
-        int ptr = 0;
-        int len = A.length;
-        int[] result = new int[len];
-        // split array A to <0 and >=0
-        while(ptr<len&&A[ptr]<0)
-            ptr++;
-        // negative
-        int neg = ptr - 1;
-        int p = 0;
-        while(neg >=0 && ptr < len){
-            if (A[neg] * A[neg] < A[ptr] * A[ptr]){
-                result[p++] = A[neg] * A[neg];
-                neg--;
+    public int[] sortedSquares(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        int index = nums.length - 1;
+        int[] result = new int[nums.length];
+        while(left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare =  nums[right]*nums[right];
+            if(leftSquare > rightSquare) {
+                result[index--] = leftSquare;
+                left++;
             }else{
-                result[p++] = A[ptr] * A[ptr];
-                ptr++;
+                result[index--] = rightSquare;
+                right--;
             }
-        }
-        while(neg >= 0){
-            result[p++] = A[neg] * A[neg];
-            neg--;
-        }
-        while(ptr<len){
-            result[p++] = A[ptr] * A[ptr];
-            ptr++;
         }
         return result;
     }
