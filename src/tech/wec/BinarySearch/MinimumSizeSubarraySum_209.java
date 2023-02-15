654e65d350c783cf8a0f6e0f29bd1bb7fc9dee64
williamericcheung <williamericcheung@outlook.com>
2023-02-15 09:39:24 +0800

    fix

--- src/tech/wec/BinarySearch/MinimumSizeSubarraySum_209.java
+++ src/tech/wec/BinarySearch/MinimumSizeSubarraySum_209.java
@@ -3,19 +3,18 @@ package tech.wec.BinarySearch;
 import java.util.Arrays;
 
 public class MinimumSizeSubarraySum_209 {
-    public int minSubArrayLen(int s, int[] nums) {
-        int ans = Integer.MAX_VALUE;
-        int n = nums.length;
-        int left = 0;
+    public int minSubArrayLen(int target, int[] nums) {
+        int min = Integer.MAX_VALUE;
         int sum = 0;
-        for (int i = 0; i < n; i++){
-            sum += nums[i];
-            while(sum >= s){
-                ans = Math.min(ans, i + 1 - left);
+        int left = 0;
+        for(int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            while(sum >= target) {
+                min = Math.min(min, (right - left + 1));
                 sum -= nums[left++];
             }
         }
-        return (ans != Integer.MAX_VALUE)? ans : 0;
+        return min != Integer.MAX_VALUE ? min: 0;
     }
 
 }
